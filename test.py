024b41270202b8a8cf8a4d60ff9d90ce77bc6a1f
lmh <dev@hilberg.eu>
2025-10-25 16:34:20 +0200

    updating test.py

--- test.py
+++ test.py
@@ -3,6 +3,7 @@
 """
 tests for usage
 """
+import pandas as pd
 from pylmtic import PyLMTic
 from pydantic import BaseModel
 
@@ -11,9 +12,19 @@ from pydantic import BaseModel
 class CityLocation(BaseModel):
     city: str
     country: str
+    year: int
 
 lm = PyLMTic(model_name="qwen", host_url="http://localhost:1234/v1")
 prompt = "Where were the Olympics held in 2012 and in 2018?"
 result = lm.run_prompt(prompt, output_type=CityLocation)
 
+print("Here we go: there are two answers, so we will get two pydantic objects")
 print(result)
+
+print("\nWe can convert them to a pandas dataframe")
+df = pd.DataFrame([r.model_dump() for r in result])
+print(df)
+
+print("\nAnd filtering for events older than 2015")
+
+print(df[df['year']>= 2015])
